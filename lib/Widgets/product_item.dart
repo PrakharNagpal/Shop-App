8ed139ec7a39f74f4dfdb8fc972d2ed6c9c6ff85
Prakhar Nagpal <53858906+PrakharNagpal@users.noreply.github.com>
2021-04-24 10:13:40 +0530

    Added a few colors and fonts

--- lib/Widgets/product_item.dart
+++ lib/Widgets/product_item.dart
@@ -13,7 +13,27 @@ class Productitem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageUrl),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.shopping_cart,
+          ),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './Screens/Product_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
