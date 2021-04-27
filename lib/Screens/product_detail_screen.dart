dd781e8af1b359e71bb27f5ebafa9fab3d8765c0
Prakhar Nagpal <53858906+PrakharNagpal@users.noreply.github.com>
2021-04-27 22:43:00 +0530

    Route

--- lib/Screens/product_detail_screen.dart
+++ lib/Screens/product_detail_screen.dart
@@ -10,8 +10,8 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    final loadedProduct =
-        Provider.of<Product_Provider>(context).findById(productId);
+    final loadedProduct = Provider.of<Product_Provider>(context, listen: false)
+        .findById(productId);
 
     return Scaffold(
       appBar: AppBar(
